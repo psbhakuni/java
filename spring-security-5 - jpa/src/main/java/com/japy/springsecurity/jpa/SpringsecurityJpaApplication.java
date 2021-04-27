9a95a25e5bc7135386083e5d0662e5890360683a
Prakash Bhakuni <psbhakuni@gmail.com>
2021-04-27 20:54:52 +0530

    Spring security JPA initial commit

---
+++ spring-security-5 - jpa/src/main/java/com/japy/springsecurity/jpa/SpringsecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.japy.springsecurity.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityJpaApplication.class, args);
+	}
+
+}
---
+++ spring-security-5 - jpa/src/main/java/com/japy/springsecurity/jpa/config/MyUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.japy.springsecurity.jpa.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
